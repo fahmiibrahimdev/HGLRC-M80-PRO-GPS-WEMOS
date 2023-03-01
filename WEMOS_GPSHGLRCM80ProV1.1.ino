6987247d23484974e233af3e8ffc35960d17f0a7
Fahmi Ibrahim <96789851+fahmiibrahimdev@users.noreply.github.com>
2023-03-01 12:42:16 +0700

    Add files via upload

---
+++ WEMOS_GPSHGLRCM80ProV1.1.ino
@@ -0,0 +1,92 @@
+#include <SoftwareSerial.h>
+#include <TinyGPS++.h>
+
+int RXPin = D3;
+int TXPin = D4;
+
+int GPSBaud = 115200;
+boolean newData = false;
+float latitude, longitude;
+int comma1, comma2, comma3, comma4, comma5, comma6;
+String inputStringGPS, latitudeString, longitudeString;
+
+SoftwareSerial gpsSerial(RXPin, TXPin);
+TinyGPSPlus gps;
+
+void setup()
+{
+  Serial.begin(115200);
+  gpsSerial.begin(GPSBaud);
+}
+
+void loop()
+{
+  while (gpsSerial.available()) 
+  {
+    char inputCharGPS = (char)gpsSerial.read();
+    inputStringGPS += inputCharGPS;
+    newData = true;
+  }
+  
+  if (newData) 
+  {
+    latitude = getLatitude();
+    longitude = getLongitude();
+    Serial.print("Latitude: ");
+    Serial.println(latitude, 6);
+    Serial.print("Longitude: ");
+    Serial.println(longitude, 6);
+    getMapsLink(latitude, longitude);
+    Serial.println("\n");
+    inputStringGPS = "";
+    newData = false;
+  }
+
+  delay(3000);
+}
+
+void indexOfComma()
+{
+  comma1 = inputStringGPS.indexOf(",");
+  comma2 = inputStringGPS.indexOf(",", comma1 + 1);
+  comma3 = inputStringGPS.indexOf(",", comma2 + 1);
+  comma4 = inputStringGPS.indexOf(",", comma3 + 1);
+  comma5 = inputStringGPS.indexOf(",", comma4 + 1);
+  comma6 = inputStringGPS.indexOf(",", comma5 + 1);
+}
+
+float getLatitude()
+{
+  indexOfComma();
+  latitudeString  = inputStringGPS.substring(comma2 + 1, comma3);
+  float latitude = latitudeString.substring(0, 2).toFloat() + latitudeString.substring(2).toFloat() / 60.0;
+  
+  if (inputStringGPS.charAt(comma3 + 1) == 'S') 
+  {
+    latitude *= -1.0;
+  }
+
+  return latitude;
+}
+
+float getLongitude()
+{
+  indexOfComma();
+  longitudeString = inputStringGPS.substring(comma4 + 1, comma5);
+  float longitude = longitudeString.substring(0, 3).toFloat() + longitudeString.substring(3).toFloat() / 60.0;
+
+  if (inputStringGPS.charAt(comma6 + 1) == 'W') 
+  {
+    longitude *= -1.0;
+  }
+
+  return longitude;
+}
+
+void getMapsLink(float latitude, float longitude)
+{
+  Serial.print("Maps Link: http://maps.google.com/maps?q=");
+  Serial.print(latitude, 6);
+  Serial.print(",");
+  Serial.print(longitude, 6);
+}
